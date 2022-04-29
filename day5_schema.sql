a4831dd6b1352328425f34666484a39ea950e01a
Fisseha Estifanos <fisseha.137@gmail.com>
2022-04-29 16:39:28 +0300

    create  table

--- day5_schema.sql
+++ day5_schema.sql
@@ -1,12 +1,12 @@
-"""CREATE TABLE IF NOT EXISTS TweetInformation(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS TweetInformation (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     created_at TEXT NOT NULL,
     source TEXT NOT NULL,
     clean_text TEXT DEFAULT NULL,
     polarity FLOAT DEFAULT NULL,
     subjectivity FLOAT DEFAULT NULL,
     language TEXT DEFAULT NULL,
-    favorite_count INTEGER DEFAULT NULL,
+    favorite_count INTEGEREGER DEFAULT NULL,
     retweet_count INTEGER DEFAULT NULL,
     original_author TEXT DEFAULT NULL,
     screen_count INTEGER NOT NULL,
@@ -15,7 +15,6 @@
     hashtags TEXT DEFAULT NULL,
     user_mentions TEXT DEFAULT NULL,
     place TEXT DEFAULT NULL,
-    place_coordinate VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (id)
+    place_coordinate TEXT DEFAULT NULL
 );
-"""
+
